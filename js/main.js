8d43e1343a282e17764911c1407943e80a9e0815
GiovanniR-S <giovannirobertssilva@outlook.com>
2021-01-02 17:06:34 -0300

    Primeiras aulas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,63 @@
-alert("Meu primeiro js");
+
+/*
+var d = new Date();
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var count;
+for(count = 0; count <= 5; count++){
+
+    console.log(count);
+
+}
+*/
+
+/*
+var count = 0;
+while(count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+
+/*
+var idade = prompt("Qual sua idade: ");
+if(idade >= 18){
+
+    alert("Maior de idade");
+
+}else{
+
+    alert("Menor de idade");
+
+}
+*/
+
+//var frutas = [{nome:"Maça", cor:"vermelha"}, {nome:"Uva", cor:"roxo"}]
+//console.log(frutas[1].nome);
+
+/*
+var fruta = {nome:"Maça", cor:"vermelha"}
+console.log(fruta.nome);]
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "perâ", "laranja"];
+//lista.push("uva");
+//lista.pop();
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+
+/*
+var name = "Giovanni";
+var idade = 19;
+var idade2 = 10;
+var frase = "Japão é melhor time"
+alert("Bem vindo " + name + " Idade: " + idade);
+alert(idade + idade2);
+console.log(frase.replace("Japão", "Brasil"));
+*/
